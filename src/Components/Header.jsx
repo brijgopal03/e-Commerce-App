0bdb7b4b0abb895bb0648d29651ab4e66baf4222
brijgopal03 <brijgopaldalmia2003@gmail.com>
2024-10-21 01:18:48 +0530

    created header file

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../"
+const Header = () => {
+  return (
+    <header>
+    <img width={100} src="https://i0.wp.com/www.dafontfree.co/wp-content/uploads/2021/11/Amazon-Logo-Font-1-scaled.jpg?resize=2560%2C1578" alt="amazon logo" />
+    <form action="">
+        <input type="text" placeholder='Search Anything' className='search'/>
+    </form>
+    <div>
+    <span>Hello Brijgopal</span>
+    <br />
+    <b></b>
+    </div>
+
+    </header>
+  )
+}
+
+export default Header
